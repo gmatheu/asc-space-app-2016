5e7fdea6efe6f42df0e20aa92aa5c41c1e2aaab2
Gonzalo Matheu <gmatheu@ascentio.com.ar>
2016-04-23 10:39:23 -0300

    Initial flask static app

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    data = {'title': 'Hello World'}
+    return render_template("index.html",
+                           title='Home',
+                           data=data)
